5a378cb9294ca212d87738b26ad29ce15d80a608
Yuliya Kogol <kogol.julia@gmail.com>
2022-02-21 23:00:30 +0300

    refactor: add a loop

--- task0.py
+++ task0.py
@@ -4,12 +4,14 @@ task = input("Введите задачу: ")
 print(date, task)
 
 #Задание 2
-list = []
+my_list = []
 for x in range(3):
     date = input("Введите дату: ")
     task = input("Введите задачу: ")
-    list.append(date + " " + task)
-print(list[0] + "\n" + list[1] + "\n" + list[2])
+    my_list.append(date + " " + task)
+
+for y in my_list:
+    print(y)
 
 #Задание 3
 dict = {}
