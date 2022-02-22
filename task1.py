c8d5c4fd9c1d693d0fcb042834b03080b9969508
Yuliya Kogol <kogol.julia@gmail.com>
2022-02-22 16:56:51 +0300

    refactor

--- task1.py
+++ task1.py
@@ -4,29 +4,32 @@ add - добавить задачу в список (название задач
 show - напечатать все добавленные задачи."""
 
 tasks = []
-today = []
-tomorrow = []
-other = []
+today = list() # today = []
+tomorrow = list()
+other = list()
 
 run = True
 
 while run:
-    command = input("Введите команду: ")
+    command = input("Введите команду\n")
     if command == "help":
         print(HELP)
     elif command == "show":
-        print(tasks, today, tomorrow, other)
+        print(tasks)
+        print("Сегодня")
+        for task in today:
+            print(task)
     elif command == "add":
         task = input("Введите название задачи: ")
         date = input("Когда выполнить задание? ")
         tasks.append(task)
         if date == "Сегодня":
-            today.append(date)
+            today.append(task)
         elif date == "Завтра":
-            tomorrow.append(date)
+            tomorrow.append(task)
         else:
-            other.append(date)
-        print("Задача добавлена")
+            other.append(task)
+        print(f"Задача {task} добавлена")
     elif command == "exit":
         print("Спасибо за использование! До свидания!")
         break
